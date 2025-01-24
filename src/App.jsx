076b19c98d1c6ae25fb8d529428735742b779f1a
Deasa092 <deasa092@gmail.com>
2025-01-24 10:34:19 +0700

    remove yang tidak terpakai

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import Dashboard from "./component/dashboard";
 import DashboardWithoutSock from "./component/DashboardWithoutSock";
 
 function App() {
